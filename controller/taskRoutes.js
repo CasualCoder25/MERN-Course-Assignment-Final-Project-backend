036043577fc6606a2711878c7167238acd386328
CasualCoder25 <h.rajanna2505@gmail.com>
2023-11-07 15:53:25 +0530

    BugFix:Routes

--- controller/taskRoutes.js
+++ controller/taskRoutes.js
@@ -45,34 +45,49 @@ taskRoutes.post("/create-task", (req, res) => {
   } = req.body
   const subject = "Reminder for task " + task_name
   const text = "Your task " + task_name + " is due at " + reminder_time + "."
-  if (completed || !reminder_time) {
-    reminder_active = false
-  }
-  const task = {
-    user_email_id: user_email_id,
-    task_name: task_name,
-    task_description: task_description,
-    star: star,
-    priority_number: priority_number,
-    reminder_active: reminder_active,
-    reminder_time: reminder_time,
-    completed: completed,
-  }
-  TasksSchema.create(task, (err, data) => {
-    if (err) {
-      res.json({ error: err, status: 500 })
-    } else {
-      const task_id = data._id
-      const from = "todolistmail23@gmail.com"
-      if (reminder_active) {
-        const timeout = getTimeout(reminder_time)
-        if (timeout > 0) {
-          emailService(from, user_email_id, subject, text, task_id, timeout)
-        }
-      }
-      res.json({ message: "Success" })
+  const valid = inputTaskValidator(
+    task_id,
+    user_email_id,
+    task_name,
+    task_description,
+    star,
+    priority_number,
+    reminder_active,
+    reminder_time,
+    completed
+  )
+  if (valid) {
+    if (completed || !reminder_time) {
+      reminder_active = false
     }
-  })
+    const task = {
+      user_email_id: user_email_id,
+      task_name: task_name,
+      task_description: task_description,
+      star: star,
+      priority_number: priority_number,
+      reminder_active: reminder_active,
+      reminder_time: reminder_time,
+      completed: completed,
+    }
+    TasksSchema.create(task, (err, data) => {
+      if (err) {
+        res.json({ error: err, status: 500 })
+      } else {
+        const task_id = data._id
+        const from = "todolistmail23@gmail.com"
+        if (reminder_active) {
+          const timeout = getTimeout(reminder_time)
+          if (timeout > 0) {
+            emailService(from, user_email_id, subject, text, task_id, timeout)
+          }
+        }
+        res.json({ message: "Success" })
+      }
+    })
+  } else {
+    res.json({ error: "Invalid input data", status: 500 })
+  }
 })
 
 // EDIT-TASK
@@ -90,40 +105,55 @@ taskRoutes.put("/edit-task", (req, res) => {
   } = req.body
   const subject = "Reminder for task " + task_name
   const text = "Your task " + task_name + " is due at " + reminder_time + "."
-  if (completed || !reminder_time) {
-    reminder_active = false
-  }
-  const task = {
-    user_email_id: user_email_id,
-    task_name: task_name,
-    task_description: task_description,
-    star: star,
-    priority_number: priority_number,
-    reminder_active: reminder_active,
-    reminder_time: reminder_time,
-    completed: completed,
-  }
-  TasksSchema.findByIdAndUpdate(
-    mongoose.Types.ObjectId(task_id),
-    {
-      $set: task,
-    },
-    (err, data) => {
-      if (err) {
-        res.json({ error: err, status: 500 })
-      } else {
-        const from = "todolistmail23@gmail.com"
-        deleteTimeoutEmail(user_email_id, task_id)
-        if (reminder_active) {
-          const timeout = getTimeout(reminder_time)
-          if (timeout > 0) {
-            emailService(from, user_email_id, subject, text, task_id, timeout)
-          }
-        }
-        res.json({ message: "Success" })
-      }
-    }
+  const valid = inputTaskValidator(
+    task_id,
+    user_email_id,
+    task_name,
+    task_description,
+    star,
+    priority_number,
+    reminder_active,
+    reminder_time,
+    completed
   )
+  if (valid) {
+    if (completed || !reminder_time) {
+      reminder_active = false
+    }
+    const task = {
+      user_email_id: user_email_id,
+      task_name: task_name,
+      task_description: task_description,
+      star: star,
+      priority_number: priority_number,
+      reminder_active: reminder_active,
+      reminder_time: reminder_time,
+      completed: completed,
+    }
+    TasksSchema.findByIdAndUpdate(
+      mongoose.Types.ObjectId(task_id),
+      {
+        $set: task,
+      },
+      (err, data) => {
+        if (err) {
+          res.json({ error: err, status: 500 })
+        } else {
+          const from = "todolistmail23@gmail.com"
+          deleteTimeoutEmail(user_email_id, task_id)
+          if (reminder_active) {
+            const timeout = getTimeout(reminder_time)
+            if (timeout > 0) {
+              emailService(from, user_email_id, subject, text, task_id, timeout)
+            }
+          }
+          res.json({ message: "Success" })
+        }
+      }
+    )
+  } else {
+    res.json({ error: "Invalid input data", status: 500 })
+  }
 })
 
 // DELETE-TASKS
@@ -142,5 +172,46 @@ taskRoutes.delete("/delete-task", (req, res) => {
     }
   )
 })
+
+// VERIFY-IFVALID
+const inputTaskValidator = (
+  task_id,
+  user_email_id,
+  task_name,
+  task_description,
+  star,
+  priority_number,
+  reminder_active,
+  reminder_time,
+  completed
+) => {
+  if (
+    task_id === undefined ||
+    user_email_id === undefined ||
+    task_name === undefined ||
+    task_description === undefined ||
+    star === undefined ||
+    priority_number === undefined ||
+    reminder_active === undefined ||
+    reminder_time === undefined ||
+    completed === undefined
+  ) {
+    return false
+  } else if (
+    task_id === null ||
+    user_email_id === null ||
+    task_name === null ||
+    task_description === null ||
+    star === null ||
+    priority_number === null ||
+    reminder_active === null ||
+    reminder_time === null ||
+    completed === null
+  ) {
+    return false
+  } else {
+    return true
+  }
+}
 
 module.exports = taskRoutes
--- readme.md
+++ readme.md
@@ -1,7 +1,4 @@
-# 0.5.1 - ToDo List backend:
+# 1.0.0 - ToDo List backend:
 
-### Pending tasks:
-
-1. edit-task route needs input verification before execution
-
-Note: Whenever a task is completed, set reminder_active to false
+- Open To Use
+- All Pending Tasks Completed XD
