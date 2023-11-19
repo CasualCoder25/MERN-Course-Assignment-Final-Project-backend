d53ff8ac6570dc4c18236ffe0498dbdddabeb8b9
CasualCoder25 <h.rajanna2505@gmail.com>
2023-11-19 20:39:56 +0530

    commit4

--- services/getTimeout.js
+++ services/getTimeout.js
@@ -1,8 +1,11 @@
 //time format: "Nov 07 2023 13:35:20"
 const getTimeout = (time) => {
   try {
+    console.log(time)
     const current = new Date()
+    console.log(current)
     const remind = new Date(time)
+    console.log(remind)
     return remind.getTime() - current.getTime()
   } catch (err) {
     return null
